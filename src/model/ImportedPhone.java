2822c59d2504f7c1ed4b0560950dad9c0b5f5972
phuhoang.06 <hoangvanphu2006@gmail.com>
2024-12-10 09:28:30 +0700

    thêm override cho các class

--- src/model/ImportedPhone.java
+++ src/model/ImportedPhone.java
@@ -2,36 +2,18 @@ package model;
 
 public class ImportedPhone extends Phone {
     private String country;  // Quốc gia xách tay
-    private String status;   // Trạng thái ("Mới" hoặc "Đã qua sử dụng")
+    private String status;   // Trạng thái ("Da sua chua" hoặc "Chua sua chua")
 
-    // Constructor
     public ImportedPhone(String id, String name, double price, int quantity, String manufacturer, String country, String status) {
         super(id, name, price, quantity, manufacturer);
         this.country = country;
         this.status = status;
     }
 
-    // Overridden method to get details of imported phone
     @Override
     public String getDetails() {
-        return "ImportedPhone [ID=" + getId() + ", Name=" + getName() + ", Price=" + getPrice() + ", Quantity=" + getQuantity() + ", Manufacturer=" + getManufacturer() + ", Country=" + country + ", Status=" + status + "]";
-    }
-
-    // Getters and Setters
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
+        return super.toString() +
+                ", Quốc gia xách tay: " + country +
+                ", Trạng thái: " + status;
     }
 }
-
--- src/model/OfficialPhone.java
+++ src/model/OfficialPhone.java
@@ -2,36 +2,18 @@ package model;
 
 public class OfficialPhone extends Phone {
     private int warrantyPeriod; // Thời gian bảo hành (tháng)
-    private String warrantyScope; // Phạm vi bảo hành ("Trong nước" hoặc "Quốc tế")
+    private String warrantyScope; // Phạm vi bảo hành ("Toan Quoc" hoặc "Quoc Te")
 
-    // Constructor
     public OfficialPhone(String id, String name, double price, int quantity, String manufacturer, int warrantyPeriod, String warrantyScope) {
         super(id, name, price, quantity, manufacturer);
         this.warrantyPeriod = warrantyPeriod;
         this.warrantyScope = warrantyScope;
     }
 
-    // Overridden method to get details of official phone
     @Override
     public String getDetails() {
-        return "OfficialPhone [ID=" + getId() + ", Name=" + getName() + ", Price=" + getPrice() + ", Quantity=" + getQuantity() + ", Manufacturer=" + getManufacturer() + ", Warranty Period=" + warrantyPeriod + " months, Warranty Scope=" + warrantyScope + "]";
-    }
-
-    // Getters and Setters
-    public int getWarrantyPeriod() {
-        return warrantyPeriod;
-    }
-
-    public void setWarrantyPeriod(int warrantyPeriod) {
-        this.warrantyPeriod = warrantyPeriod;
-    }
-
-    public String getWarrantyScope() {
-        return warrantyScope;
-    }
-
-    public void setWarrantyScope(String warrantyScope) {
-        this.warrantyScope = warrantyScope;
+        return super.toString() +
+                ", Thời gian bảo hành: " + warrantyPeriod + " tháng" +
+                ", Phạm vi bảo hành: " + warrantyScope;
     }
 }
-
